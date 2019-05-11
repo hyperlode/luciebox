d660de375107a1011066454f41918384c3f6a74a
lode <lodeameije@gmail.com>
2019-05-11 16:30:01 -0700

    tidied up. Buttons matching numbers. R2R ladder working for buttons without NC position. (I used a pulldown resistor, 1/10th of the R value of the R-2R ladder. Works for limited number of buttons. I think 3 or 4 buttons is the maximum.

--- Pretbak.ino
+++ Pretbak.ino
@@ -1,7 +1,6 @@
 #include "PotentioSelector.h"
 #include "ButtonsDacR2r.h"
 #include "BinaryInput.h"
-#include "Button.h"
 
 // Lode Ameije 2019-05
 // Pretbak is a busy box for my newborn niece
@@ -14,75 +13,49 @@
 #define PIN_BUTTONS_2 A2
 #define PIN_POTENTIO A3
 
-#define PIN_BUTTON_DOUBLE_LATCH 2
-#define PIN_BUTTON_BIG_LATCH 3
-
 #define BINARY_INPUTS_COUNT 8
 #define BUTTONS_1_COUNT 4
-#define BUTTONS_1_TO_BINARY_INPUT_OFFSET 4
+#define BUTTONS_1_TO_BINARY_INPUT_OFFSET 3
+
 #define BUTTONS_2_COUNT 3
-#define BUTTONS_2_TO_BINARY_INPUT_OFFSET 1
+#define BUTTONS_2_TO_BINARY_INPUT_OFFSET 0
 
-
-//#define BUT_BIG 0
-//#define TILT 1
-//#define BUT_L 2
-//#define BUT_R 3
-
-#define TILT_SWITCH_VERTICAL 0
-#define BUTTON_LATCHING_BIG_RED 1
-#define BUTTON_LATCHING_SMALL_RED_LEFT 2
-#define BUTTON_LATCHING_SMALL_RED_RIGHT 3
-#define BUTTON_LATCHING_YELLOW 4
-#define BUTTON_MOMENTARY_RED 5
-#define BUTTON_MOMENTARY_GREEN 6
-#define BUTTON_MOMENTARY_BLUE 7
+#define BUTTON_LATCHING_BIG_RED 2
+#define BUTTON_LATCHING_SMALL_RED_LEFT 1
+#define BUTTON_LATCHING_SMALL_RED_RIGHT 0
+#define BUTTON_LATCHING_YELLOW 3
+#define BUTTON_MOMENTARY_RED 4
+#define BUTTON_MOMENTARY_GREEN 5
+#define BUTTON_MOMENTARY_BLUE 6
 
 BinaryInput binaryInputs[BINARY_INPUTS_COUNT];
 
 PotentioSelector selectorDial;
 ButtonsDacR2r buttons_1;
-ButtonsDacR2r buttons_2;
-Button buttons[4];
+ButtonsDacR2r buttons_2; // buttons with normally closed. this is a problem for the R-2R ladder. instead, I used a pull down resistor to ground at the switch. so: ON = 5V, OFF = GND over 1Kohm. 10K, 20K R2Rladder.  will only work for limited number of buttons.
 
 void refresh(){
   selectorDial.refresh();
   buttons_1.refresh();
   buttons_2.refresh();
 
-  
-//  for(uint8_t i=2;i<4;i++){
-//    buttons[i].twoButtonsOnePinHandler();
-//    buttons[i].refresh();
-//    binaryInputs[i].setValue(buttons[i].getState());
-//  }
-//    Serial.println(buttons_2.getButtonsValueAnalog());
-
   if (buttons_2.getValueChangedEdge()) {
-    Serial.println("analog 2in buttons:");
-    Serial.println(buttons_2.getButtonsValueAnalog());
-    Serial.println(buttons_2.getButtonsValue());
-    
-      for (uint8_t i=0; i< BUTTONS_2_COUNT; i++){
-        binaryInputs[BUTTONS_2_TO_BINARY_INPUT_OFFSET + i].setValue(buttons_2.getButtonValueByIndex(i));
-        //Serial.println("------");
-      }
-    
-    
+    //Serial.println("analog 2in buttons:");
+    //Serial.println(buttons_2.getButtonsValueAnalog());
+    //Serial.println(buttons_2.getButtonsValue());
+    for (uint8_t i=0; i< BUTTONS_2_COUNT; i++){
+      binaryInputs[BUTTONS_2_TO_BINARY_INPUT_OFFSET + i].setValue(buttons_2.getButtonValueByIndex(i));
+    }
   }
 
   if (buttons_1.getValueChangedEdge()) {
     //Serial.println("analog in buttons:");
-//    Serial.println(buttons_1.getButtonsValueAnalog());
+    //Serial.println(buttons_1.getButtonsValueAnalog());
     //Serial.println(buttons_1.getButtonsValue());
     for (uint8_t i=0; i< BUTTONS_1_COUNT; i++){
       binaryInputs[BUTTONS_1_TO_BINARY_INPUT_OFFSET + i].setValue(buttons_1.getButtonValueByIndex(i));
-      Serial.println("------");
-      Serial.println(i);
-//      Serial.println(binaryInputs[i].getValue());
-//      Serial.println(binaryInputs[i].getValueChanged());
-//      Serial.println(binaryInputs[i].getEdgeUp());
-//      Serial.println(binaryInputs[i].getEdgeDown());
+      //Serial.println("------");
+      //Serial.println(i);
     }
   }
  
@@ -116,7 +89,6 @@ void refresh(){
 
 void mode_auto(){
 
-  
 }
 
 
@@ -125,20 +97,6 @@ void setup() {
   selectorDial.setPin(PIN_SELECTOR_DIAL);
   buttons_1.setPin(PIN_BUTTONS_1,BUTTONS_1_COUNT);
   buttons_2.setPin(PIN_BUTTONS_2,BUTTONS_2_COUNT);
-
-  
-//  buttons[BUT_L].setPin(PIN_BUTTON_DOUBLE_LATCH);
-//  buttons[BUT_R].setPin(PIN_BUTTON_DOUBLE_LATCH);
-
-  //buttons[BUT_BIG].setPin(PIN_BUTTON_BIG_LATCH);
-  //buttons[TILT].setPin(PIN_BUTTON_BIG_LATCH);
-
-//  buttons[BUT_L].setTwoButtonsOnePinButtonConnectedToGnd(true);
-//  buttons[BUT_R].setTwoButtonsOnePinButtonConnectedToGnd(false);
-  
-  //buttons[BUT_BIG].setTwoButtonsOnePinButtonConnectedToGnd(false);
-  //buttons[TILT].setTwoButtonsOnePinButtonConnectedToGnd(true);
-
 
   Serial.begin(9600);
 }
