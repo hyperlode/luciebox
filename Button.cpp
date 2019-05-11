aef5b047aa16abca91d1ec90a7a93177f0fb43df
lode <lodeameije@gmail.com>
2019-05-11 14:22:05 -0700

    should have worked, but doesn't

---
+++ Button.cpp
@@ -0,0 +1,365 @@
+#include "Button.h"
+#include "Arduino.h"
+
+//empty constructor
+Button::Button(){
+	//check if multiple buttons per Pin!!!
+	// this->twoButtonsOnePinButtonConnectedToGnd = true;
+	//setBit(&this->boolContainer, true, BITLOCATION_TWOBUTTONSONEPINBUTTONCONNECTEDTOGND); //default button connected to gnd for on.
+	setTwoButtonsOnePinButtonConnectedToGnd(true); //default button connected to gnd for on.
+	setButtonPartOfTwoButtonsPerPinSystem(true);//default button is part of a two button per pin system.
+	
+};
+
+
+//void twoButtonsHandler(){
+// http://www.arduino.cc/en/Tutorial/TwoSwitchesOnePin
+
+/*
+ * Read_Two_Switches_On_One_Pin
+ * Read two pushbutton switches or one center-off toggle switch with one Arduino pin
+ * Paul Badger 2008 
+ * From an idea in EDN (Electronic Design News)
+ *
+ * Exploits the pullup resistors available on each I/O and analog pin
+ * The idea is that the 200K resistor to ground will cause the input pin to report LOW when the 
+ * (20K) pullup resistor is turned off, but when the pullup resistor is turned on, 
+ * it will overwhelm the 200K resistor and the pin will report HIGH.
+ *
+ * Schematic Diagram    ( can't belive I drew this funky ascii schematic )     
+ *
+ *
+ *                             +5 V
+ *                                |
+ *                                \
+ *                                /   
+ *                                \    10K
+ *                                /
+ *                                \
+ *                                |
+ *                               /    switch 1  or 1/2 of center-off toggle or slide switch
+ *                              /       
+ *                                |
+ *            digital pin ________+_____________/\/\/\____________   ground              
+ *                                |               
+ *                                |               200K to 1M  (not critical)
+ *                               /   
+ *                              /        switch 2 or 1/2 of center-off toggle or slide switch
+ *                                |
+ *                                |
+ *                              _____   
+ *                               ___     ground
+ *                                _
+ *
+ */
+
+
+// #define swPin 2                 // pin for input  - note: no semicolon after #define
+// int stateA, stateB;             // variables to store pin states
+// int sw1, sw2;                   // variables to represent switch states 
+
+// void setup()                   
+// {
+   // Serial.begin(9600);
+// }
+
+// void loop()            
+// {
+   // digitalWrite(swPin, LOW);                   // make sure the puillup resistors are off
+   // stateA = digitalRead(swPin);
+   // digitalWrite(swPin, HIGH);                  // turn on the puillup resistors
+   // stateB = digitalRead(swPin);
+
+   // if ( stateA == 1 && stateB == 1 ){          // both states HIGH - switch 1 must be pushed
+      // sw1 = 1;
+      // sw2 = 0;
+   // }
+   // else if ( stateA == 0 && stateB == 0 ){     // both states LOW - switch 2 must be pushed
+      // sw1 = 0;
+      // sw2 = 1;
+   // }
+   // else{                                       // stateA HIGH and stateB LOW 
+      // sw1 = 0;                                 // no switches pushed - or center-off toggle in middle position
+      // sw2 = 0;
+   // }  
+
+   // Serial.print(sw1);
+   // Serial.print("    ");     // pad some spaces to format print output
+   // Serial.println(sw2);
+
+   // delay(100);  
+//}
+
+//};
+
+#ifdef EXTRAPIN
+void Button::setPin( byte pin1, byte pin2 ){
+	this->pin1 = pin1;
+	pinMode(pin1, INPUT);
+	digitalWrite(pin1, HIGH);  // set pullup on pin
+	this->pin2 = pin2;
+	if (pin2 != NO_EXTRA_INPUTPIN_SET){
+		pinMode(pin2, INPUT);
+		digitalWrite(pin2, HIGH);  // set pullup on pin
+	}
+};
+
+void Button::unsetExtraPin(byte pin){
+	//check if extra pin is the pin we want to unset, if so, do it!
+	if (pin2 == pin){
+		this->pin2 = NO_EXTRA_INPUTPIN_SET;
+	}
+}
+#else
+
+void Button::setButtonPartOfTwoButtonsPerPinSystem(bool isPartOfTwoButtonsPerPin){
+	setBit(&this->boolContainer, isPartOfTwoButtonsPerPin, BITLOCATION_BUTTONISPARTOFTWOBUTTONSPERPINSYSTEM); 
+}
+
+void Button::setPin( byte pin1){
+	this->pin1 = pin1;
+	//Serial.println(pin1);
+	pinMode(pin1, INPUT);
+	digitalWrite(pin1, HIGH);  // set pullup on pin
+	
+	
+};
+
+#endif
+
+void Button::setTwoButtonsOnePinButtonConnectedToGnd(bool connectedToGnd){
+	//we are talking about pin1 here! this has nothing to do with pin2 (pin2 comes from an external source...not involved in the multiplexing)
+	// this->twoButtonsOnePinButtonConnectedToGnd = connectedToGnd;
+	setBit(&this->boolContainer, connectedToGnd, BITLOCATION_TWOBUTTONSONEPINBUTTONCONNECTEDTOGND);
+}
+
+void Button::oneButtonHandler(){
+	// //connected to ground is "on"
+	// if (this-> pin2 == NO_EXTRA_INPUTPIN_SET){
+		// this->state =  (not digitalRead(this->pin1)  );//  not digitalRead(this->pin2) ); //
+	// }
+	// else{
+		// //state =  (not digitalRead(this->pin2)  );//  not digitalRead(this->pin2) ); //
+		// this->state =  (not digitalRead(this->pin1) || not digitalRead(this->pin2) );//  not digitalRead(this->pin2) ); //
+	// }
+	// refresh();
+	twoButtonsOnePinHandler();
+}
+
+void Button::twoButtonsOnePinHandler(){
+	
+	if ( getBit(&this->boolContainer, BITLOCATION_BUTTONISPARTOFTWOBUTTONSPERPINSYSTEM)){
+		digitalWrite(this->pin1, HIGH);                  // turn on the puillup resistors
+		delay(2);
+		bool stateB = digitalRead(this->pin1);
+		delay(20);
+    if (stateB){
+      //Serial.println("BHIGH");
+    }
+    
+    
+		digitalWrite(this->pin1, LOW);                   // make sure the puillup resistors are off
+		delay(1);
+   
+		bool stateA = digitalRead(this->pin1); 
+		stateA = digitalRead(this->pin1);  //set here two times, we could also use a small delay.... if not, the reading is not good, I think it needs time to come down after the pullup has been switched off!
+
+    if (stateA){
+      //Serial.println("AHIGH");
+    }
+    
+    
+		///////pinMode(5, INPUT_PULLUP); // Enable internal pull-up resistor on pin 5
+		
+		//port manipulation    http://www.arduino.cc/en/Reference/PortManipulation
+		
+		// CLR(PORTB, 0) ;     SET(PORTB, 0);
+		// http://www.billporter.info/2010/08/18/ready-set-oscillate-the-fastest-way-to-change-arduino-pins/
+		
+		// if ( stateA == 1 && stateB == 1 ){          // both states HIGH - switch1 connected to V+ must be pushed
+			// if (this ->twoButtonsOnePinButtonConnectedToGnd ){
+				// this->state = false;
+			// }else{
+				// this->state = true;
+			// }
+			// // sw1 = 1;
+			// // sw2 = 0;
+		// }else if ( stateA == 0 && stateB == 0 ){     // both states LOW - switch2 connected to GND must be pushed
+			// if (this ->twoButtonsOnePinButtonConnectedToGnd ){
+				// this->state = true;
+			// }else{
+				// this->state = false;
+			// }
+		
+			// // sw1 = 0;
+			// // sw2 = 1;
+		// }else{                                       // stateA HIGH and stateB LOW 
+			// this->state = false;
+			// // sw1 = 0;                                 // no switches pushed - or center-off toggle in middle position
+			// // sw2 = 0;
+		// }  
+		
+		//short notation for above: (to test if above works)
+		// this->state = (stateA == 1 && stateB == 1 && !this ->twoButtonsOnePinButtonConnectedToGnd) || (stateA == 0 && stateB == 0 && this ->twoButtonsOnePinButtonConnectedToGnd);
+		setBit(&this->boolContainer, (stateA == 1 && stateB == 1 && !getBit(&this->boolContainer, BITLOCATION_TWOBUTTONSONEPINBUTTONCONNECTEDTOGND)) || (stateA == 0 && stateB == 0 && getBit(&this->boolContainer, BITLOCATION_TWOBUTTONSONEPINBUTTONCONNECTEDTOGND)), BITLOCATION_STATE) ;
+		//Serial.println(
+		//  (stateA == 1 && stateB == 1 && !getBit(&this->boolContainer, BITLOCATION_TWOBUTTONSONEPINBUTTONCONNECTEDTOGND))
+		//  || (stateA == 0 && stateB == 0 && getBit(&this->boolContainer, BITLOCATION_TWOBUTTONSONEPINBUTTONCONNECTEDTOGND))
+		//  );
+		
+	#ifdef EXTRAPIN	
+		//check for extra pin...
+		if (this->pin2 != NO_EXTRA_INPUTPIN_SET){
+			setBit(&this->boolContainer, getState() || not digitalRead(this->pin2),BITLOCATION_STATE);
+			// this->state = this->state || not digitalRead(this->pin2);
+		}
+	#endif
+	}else if (this->pin1 !=PIN_FAKE){
+		setBit(&this->boolContainer, !digitalRead(this->pin1),BITLOCATION_STATE);
+		// Serial.println("press but");
+		// Serial.println(digitalRead(this->pin1));
+	}
+}
+
+bool Button::getStateEdgeUp(){
+	//edge detection
+	if (!getPreviousState() && getState()){
+		return true;
+		//this->timeLastPress = millis();
+	}else{
+		return false;
+	}
+}
+
+bool Button::getStateEdgeDown(){
+	if (getPreviousState() && !getState()){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+bool Button::getState(){
+	return getBit(&this->boolContainer, BITLOCATION_STATE);
+}
+
+bool Button::getPreviousState(){
+	return getBit(&this->boolContainer, BITLOCATION_PREVIOUSSTATE);
+}
+
+void Button::refresh(){
+	//store previous state
+	// this->previousState = this->state;
+	setBit(&this->boolContainer, getState(), BITLOCATION_PREVIOUSSTATE);
+	
+	// //edge detection
+	// if (previousState == 0 and this->state == 1){
+		// this->stateEdgeUp = 1 ;
+		// //this->timeLastPress = millis();
+	// }
+
+	// if (this->previousState == 1 and this->state == 0){
+		// this->stateEdgeDown = 1;
+
+	// }
+	// this->previousState = this->state;
+	//update time elapsed since last edge up
+	//this->timeSinceLastPressMillis = millis() - this->timeLastPress;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Pretbak.ino
+++ Pretbak.ino
@@ -1,6 +1,7 @@
 #include "PotentioSelector.h"
 #include "ButtonsDacR2r.h"
 #include "BinaryInput.h"
+#include "Button.h"
 
 // Lode Ameije 2019-05
 // Pretbak is a busy box for my newborn niece
@@ -13,27 +14,58 @@
 #define PIN_BUTTONS_2 A2
 #define PIN_POTENTIO A3
 
-#define BINARY_INPUTS_COUNT 4
+#define PIN_BUTTON_DOUBLE_LATCH 2
+#define PIN_BUTTON_BIG_LATCH 3
+
+#define BINARY_INPUTS_COUNT 8
 #define BUTTONS_1_COUNT 4
-#define BUTTONS_1_TO_BINARY_INPUT_OFFSET 0
+#define BUTTONS_1_TO_BINARY_INPUT_OFFSET 4
+
+#define BUT_BIG 0
+#define TILT 1
+#define BUT_L 2
+#define BUT_R 3
 
 #define BUTTON_LATCHING_BIG_RED 0
-#define BUTTON_LATCHING_SMALL_RED_LEFT 1
+#define TILT_SWITCH_VERTICAL 1
 #define BUTTON_LATCHING_SMALL_RED_LEFT 2
-#define BUTTON_LATCHING_YELLOW 3
-#define BUTTON_MOMENTARY_RED 4
-#define BUTTON_MOMENTARY_GREEN 5
-#define BUTTON_MOMENTARY_BLUE 6
+#define BUTTON_LATCHING_SMALL_RED_RIGHT 3
+#define BUTTON_LATCHING_YELLOW 4
+#define BUTTON_MOMENTARY_RED 5
+#define BUTTON_MOMENTARY_GREEN 6
+#define BUTTON_MOMENTARY_BLUE 7
 
 BinaryInput binaryInputs[BINARY_INPUTS_COUNT];
 
 PotentioSelector selectorDial;
 ButtonsDacR2r buttons_1;
+Button buttons[4];
 
 void refresh(){
   selectorDial.refresh();
   buttons_1.refresh();
   
+  for(uint8_t i=0;i<4;i++){
+    buttons[i].twoButtonsOnePinHandler();
+    buttons[i].refresh();
+    binaryInputs[i].setValue(buttons[i].getState());
+  }
+
+  if (buttons_1.getValueChangedEdge()) {
+    Serial.println("analog in buttons:");
+//    Serial.println(buttons_1.getButtonsValueAnalog());
+    //Serial.println(buttons_1.getButtonsValue());
+    for (uint8_t i=0; i< BUTTONS_1_COUNT; i++){
+      binaryInputs[BUTTONS_1_TO_BINARY_INPUT_OFFSET + i].setValue(buttons_1.getButtonValueByIndex(i));
+      Serial.println("------");
+      Serial.println(i);
+//      Serial.println(binaryInputs[i].getValue());
+//      Serial.println(binaryInputs[i].getValueChanged());
+//      Serial.println(binaryInputs[i].getEdgeUp());
+//      Serial.println(binaryInputs[i].getEdgeDown());
+    }
+  }
+ 
   for(uint8_t i=0;i<BINARY_INPUTS_COUNT;i++){
     if (binaryInputs[i].getEdgeUp()){
       Serial.println(i);
@@ -46,24 +78,7 @@ void refresh(){
     binaryInputs[i].refresh();
   }
 //  Serial.println("papa help "); // display this PAPA - HELP - PAPA - ... on screen when errors.
-
-  if (buttons_1.getValueChangedEdge()) {
-    //Serial.println("analog in buttons:");
-//    Serial.println(buttons_1.getButtonsValueAnalog());
-    //Serial.println(buttons_1.getButtonsValue());
-    for (uint8_t i=0; i< BUTTONS_1_COUNT; i++){
-      binaryInputs[BUTTONS_1_TO_BINARY_INPUT_OFFSET + i].setValue(buttons_1.getButtonValueByIndex(i));
-//      Serial.println("------");
-//      Serial.println(i);
-//      Serial.println(binaryInputs[i].getValue());
-//      Serial.println(binaryInputs[i].getValueChanged());
-//      Serial.println(binaryInputs[i].getEdgeUp());
-//      Serial.println(binaryInputs[i].getEdgeDown());
-    }
-
-
-  }
-  
+ 
   if (selectorDial.getValueChangedEdge()) {
 //    Serial.println(selectorDial.getSelectorValue());
     switch (selectorDial.getSelectorValue()) {
@@ -89,6 +104,19 @@ void setup() {
   // put your setup code here, to run once:
   selectorDial.setPin(PIN_SELECTOR_DIAL);
   buttons_1.setPin(PIN_BUTTONS_1,BUTTONS_1_COUNT);
+  
+  buttons[BUT_L].setPin(PIN_BUTTON_DOUBLE_LATCH);
+  buttons[BUT_R].setPin(PIN_BUTTON_DOUBLE_LATCH);
+
+  buttons[BUT_BIG].setPin(PIN_BUTTON_BIG_LATCH);
+  buttons[TILT].setPin(PIN_BUTTON_BIG_LATCH);
+
+  buttons[BUT_L].setTwoButtonsOnePinButtonConnectedToGnd(true);
+  buttons[BUT_R].setTwoButtonsOnePinButtonConnectedToGnd(false);
+  
+  buttons[BUT_BIG].setTwoButtonsOnePinButtonConnectedToGnd(false);
+  buttons[TILT].setTwoButtonsOnePinButtonConnectedToGnd(true);
+
 
   Serial.begin(9600);
 }
